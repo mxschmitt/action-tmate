04c73044e0fd6cbed37de0cbdd0321d8b4359775
Max Schmitt <max@schmitt.mx>
2020-11-04 16:49:48 +0100

    Support Windows via MSYS2 (#27)
    #26
    The MSYS2 project just added support for `tmate`; Might just as well use
    it (especially because the reason `tmate` was added was to support
    `action-tmate`).
    
    See https://github.com/msys2/MSYS2-packages/pull/2211 for details about
    MSYS2 adding support for `tmate`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,14 @@ import { spawn } from 'child_process'
 
 export const execShellCommand = (cmd) => {
   return new Promise((resolve, reject) => {
-    const process = spawn(cmd, [], { shell: true })
+    const process = global.process.platform !== "win32" ?
+      spawn(cmd, [], { shell: true }) :
+      spawn("C:\\msys64\\usr\\bin\\bash.exe", [ "-lc", cmd ], {
+        env: {
+          "CHERE_INVOKING": "1", /* do not `cd` to home */
+          "MSYSTEM": "MINGW64", /* include the MINGW programs in C:/msys64/mingw64/bin/ */
+        }
+      })
     let stdout = ""
     process.stdout.on('data', (data) => {
       console.log(data.toString());
--- src/index.test.js
+++ src/index.test.js
@@ -18,12 +18,18 @@ describe('Tmate GitHub integration', () => {
     })
   });
 
-  it('should skip for windows', async () => {
+  it('should handle the main loop for Windows', async () => {
     Object.defineProperty(process, "platform", {
       value: "win32"
     })
+    core.getInput.mockReturnValue("true")
+    const customConnectionString = "foobar"
+    execShellCommand.mockReturnValue(Promise.resolve(customConnectionString))
     await run()
-    expect(core.info).toHaveBeenCalledWith('Windows is not supported by tmate, skipping...');
+    expect(execShellCommand).toHaveBeenNthCalledWith(1, "pacman -Sy --noconfirm tmate")
+    expect(core.info).toHaveBeenNthCalledWith(1, `WebURL: ${customConnectionString}`);
+    expect(core.info).toHaveBeenNthCalledWith(2, `SSH: ${customConnectionString}`);
+    expect(core.info).toHaveBeenNthCalledWith(3, "Existing debugging session because '/continue' file was created");
   });
   it('should be handle the main loop for linux', async () => {
     Object.defineProperty(process, "platform", {
