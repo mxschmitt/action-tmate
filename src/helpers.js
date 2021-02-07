f1b22892f5b1164a8c0e2d0e4bd8e7e7f96958ec
Max Schmitt <max@schmitt.mx>
2021-02-07 09:15:54 +0100

    fix(win): inherit base environment variables (#48)

--- src/helpers.js
+++ src/helpers.js
@@ -7,25 +7,27 @@ import { spawn } from 'child_process'
  */
 export const execShellCommand = (cmd) => {
   return new Promise((resolve, reject) => {
-    const process = global.process.platform !== "win32" ?
+    const proc = process.platform !== "win32" ?
       spawn(cmd, [], { shell: true }) :
-      spawn("C:\\msys64\\usr\\bin\\bash.exe", [ "-lc", cmd ], {
+      spawn("C:\\msys64\\usr\\bin\\bash.exe", ["-lc", cmd], {
         env: {
+          ...process.env,
+          "MSYS2_PATH_TYPE": "inherit", /* Inherit previous path */
           "CHERE_INVOKING": "1", /* do not `cd` to home */
           "MSYSTEM": "MINGW64", /* include the MINGW programs in C:/msys64/mingw64/bin/ */
         }
       })
     let stdout = ""
-    process.stdout.on('data', (data) => {
+    proc.stdout.on('data', (data) => {
       console.log(data.toString());
       stdout += data.toString();
     });
 
-    process.stderr.on('data', (data) => {
+    proc.stderr.on('data', (data) => {
       console.error(data.toString());
     });
 
-    process.on('exit', (code) => {
+    proc.on('exit', (code) => {
       if (code !== 0) {
         reject(new Error(code ? code.toString() : undefined))
       }
