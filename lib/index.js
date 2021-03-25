mxschmitt/action-tmate

commit 03e13f10cd0b4e4d7002dbb0f55e7dd92b7fb9d6

fix: turn `errexit` off in the shell sessions (#64)
* DRY up the `tmate` invocations

We always pass the `-S /tmp/tmate.sock` option; Let's not repeat this
(and risk forgetting it anywhere, or risk typos, or... something).

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

* Work around Actions adding `set -e` to the Bash profile

When `set -e` (or more verbosely, `set -o errexit`) is in effect, any
failing command will automatically exit the shell. This is quite
convenient for scripts.

It is quite awful, however, for interactive sessions: even a tiny typo
will send the session to the happy hunting ground.

In fact, it is actually not _all_ that good an option to use, anyway,
see https://mywiki.wooledge.org/BashFAQ/105 for more details.

Therefore, no `/etc/profile` will set this, as it would render the
interactive sessions useless.

However, `conda-incubator/setup-miniconda` does this. Granted, the
assumption that GitHub workflows do not run interactively is an easy one
to make. And it was correct... until `action-tmate` came along.

Let's work around this by _specifically_ turning off `errexit`.

Since we do not want to add hackery on top of hackery, we will not edit
`~/.profile` but instead write our own rcfile and pass it onto the
default command `tmate` runs.

This fixes https://github.com/mxschmitt/action-tmate/issues/37.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

lib/index.js
@@ -9679,14 +9679,20 @@ async function run() {
       newSessionExtra = `-a "${authorizedKeysPath}"`
     }
 
+    const tmate = `${tmateExecutable} -S /tmp/tmate.sock`;
+
+    // Work around potential `set -e` commands in `~/.profile` (looking at you, `setup-miniconda`!)
+    external_fs_default().writeFileSync('/tmp/tmate.bashrc', 'set +e\n');
+    const setDefaultCommand = `set-option -g default-command "bash --rcfile /tmp/tmate.bashrc" \\;`;
+
     core.debug("Creating new session")
-    await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock ${newSessionExtra} new-session -d`);
-    await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock wait tmate-ready`);
+    await execShellCommand(`${tmate} ${setDefaultCommand} ${newSessionExtra} new-session -d`);
+    await execShellCommand(`${tmate} wait tmate-ready`);
     console.debug("Created new session successfully")
 
     core.debug("Fetching connection strings")
-    const tmateSSH = await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock display -p '#{tmate_ssh}'`);
-    const tmateWeb = await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock display -p '#{tmate_web}'`);
+    const tmateSSH = await execShellCommand(`${tmate} display -p '#{tmate_ssh}'`);
+    const tmateWeb = await execShellCommand(`${tmate} display -p '#{tmate_web}'`);
 
     console.debug("Entering main loop")
     while (true) {

src/index.js
@@ -68,14 +68,20 @@ export async function run() {
       newSessionExtra = `-a "${authorizedKeysPath}"`
     }
 
+    const tmate = `${tmateExecutable} -S /tmp/tmate.sock`;
+
+    // Work around potential `set -e` commands in `~/.profile` (looking at you, `setup-miniconda`!)
+    fs.writeFileSync('/tmp/tmate.bashrc', 'set +e\n');
+    const setDefaultCommand = `set-option -g default-command "bash --rcfile /tmp/tmate.bashrc" \\;`;
+
     core.debug("Creating new session")
-    await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock ${newSessionExtra} new-session -d`);
-    await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock wait tmate-ready`);
+    await execShellCommand(`${tmate} ${setDefaultCommand} ${newSessionExtra} new-session -d`);
+    await execShellCommand(`${tmate} wait tmate-ready`);
     console.debug("Created new session successfully")
 
     core.debug("Fetching connection strings")
-    const tmateSSH = await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock display -p '#{tmate_ssh}'`);
-    const tmateWeb = await execShellCommand(`${tmateExecutable} -S /tmp/tmate.sock display -p '#{tmate_web}'`);
+    const tmateSSH = await execShellCommand(`${tmate} display -p '#{tmate_ssh}'`);
+    const tmateWeb = await execShellCommand(`${tmate} display -p '#{tmate_web}'`);
 
     console.debug("Entering main loop")
     while (true) {

src/index.test.js
@@ -9,6 +9,7 @@ jest.mock("fs", () => ({
   mkdirSync: () => true,
   existsSync: () => true,
   unlinkSync: () => true,
+  writeFileSync: () => true
 }));
 jest.mock('./helpers');
 import { execShellCommand } from "./helpers"

commit 50f92a9f51df9efa4cf407e2b03d80a95ebc0833

fix: limit-access-to-actor functionality (#66)
This was a regression introduced in #64 which broke the limit-access-to-actor functionality.
The order of the input parameters seems important, since first the parameters from tmate come and then from tmux.
Fixes #65

lib/index.js
@@ -9686,7 +9686,7 @@ async function run() {
     const setDefaultCommand = `set-option -g default-command "bash --rcfile /tmp/tmate.bashrc" \\;`;
 
     core.debug("Creating new session")
-    await execShellCommand(`${tmate} ${setDefaultCommand} ${newSessionExtra} new-session -d`);
+    await execShellCommand(`${tmate} ${newSessionExtra} ${setDefaultCommand} new-session -d`);
     await execShellCommand(`${tmate} wait tmate-ready`);
     console.debug("Created new session successfully")
 

src/index.js
@@ -75,7 +75,7 @@ export async function run() {
     const setDefaultCommand = `set-option -g default-command "bash --rcfile /tmp/tmate.bashrc" \\;`;
 
     core.debug("Creating new session")
-    await execShellCommand(`${tmate} ${setDefaultCommand} ${newSessionExtra} new-session -d`);
+    await execShellCommand(`${tmate} ${newSessionExtra} ${setDefaultCommand} new-session -d`);
     await execShellCommand(`${tmate} wait tmate-ready`);
     console.debug("Created new session successfully")
 