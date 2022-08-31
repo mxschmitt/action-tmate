10c41a8b33d7eb40b011d3b16f81ef56b30d70d2
galargh <piotr.galar@gmail.com>
2022-08-31 10:30:25 +0200

    fix: import core in post.js

--- src/post.js
+++ src/post.js
@@ -1,4 +1,5 @@
 import { waitUntilDebuggingSessionExit } from "./helpers";
+import * as core from "@actions/core";
 
 if (core.getInput("wait-in-post") !== "false") {
   waitUntilDebuggingSessionExit();
